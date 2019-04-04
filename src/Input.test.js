061af112fbee8ab200eb2175195151f01983e6eb
Sunil Pai <threepointone@fb.com>
2019-04-04 13:53:54 +0100

    fix with async act

--- src/Input.test.js
+++ src/Input.test.js
@@ -1,21 +1,20 @@
 import React from 'react';
 import { render, fireEvent, cleanup } from 'react-testing-library';
 import Input from './Input';
+import {act} from 'react-dom/test-utils'
 
 afterEach(cleanup);
 
 jest.useFakeTimers();
 
-test('Input using hooks', done => {
+test('Input using hooks', async () => {
   const w = render(<Input />);
-
-  fireEvent.change(w.getByTestId('foo'), {
-    target: { value: 'bar' },
-  });
-
-  process.nextTick(() => {
-    expect(w.getByTestId('foo').value).toEqual('bar');
-    expect(w.getByTestId('check').textContent).toEqual('isBar: yes');
-    done();
-  });
+  await act(async () => {
+    fireEvent.change(w.getByTestId('foo'), {
+      target: { value: 'bar' },
+    });
+  })
+  
+  expect(w.getByTestId('foo').value).toEqual('bar');
+  expect(w.getByTestId('check').textContent).toEqual('isBar: yes');
 });
